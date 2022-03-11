3af3f10183f2109cca9e87d38b1bff7bc8c22895
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-03-11 02:41:51 -0500

    Set up Python testing architecture
    Configured Python testing with `unittest`

---
+++ run_tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Import Python test module and test classes
+import unittest
+from tests import *
+
+# Run tests
+if (__name__ == '__main__'):
+    unittest.main(verbosity=2, catchbreak=True)
