46607da0353da0e2c104f37726f384bdb644afd9
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-06-25 13:17:11 -0400

    Add package version

--- mahautils/__init__.py
+++ mahautils/__init__.py
@@ -2,3 +2,11 @@
 
 from . import exceptions
 from . import parsers
+
+
+# PROGRAM VERSION ------------------------------------------------------------
+VERSION_MAJOR = 0
+VERSION_MINOR = 0
+VERSION_PATCH = 0
+
+__version__ = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
