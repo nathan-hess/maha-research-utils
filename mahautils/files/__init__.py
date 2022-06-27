d424d01b46814c281e7d881cd53b195c4fb84dde
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-06-26 22:23:26 -0400

    Create class for processing binary files

--- mahautils/files/__init__.py
+++ mahautils/files/__init__.py
@@ -3,4 +3,5 @@ This module provides general classes that can be used for performing
 read and write operations on files.
 """
 
+from .binaryfile import BinaryFile
 from .files import File
---
+++ mahautils/files/binaryfile.py
@@ -0,0 +1,9 @@
+"""
+This module is used to process binary (non-text) files.
+"""
+
+from .files import File
+
+
+class BinaryFile(File):
+    """Base class for processing binary files"""
