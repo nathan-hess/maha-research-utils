08def8e8ae366164f18b0afca9d658dcf31e56d7
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-07-02 11:26:01 -0400

    Create test utility class that can capture text printed to the terminal

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,23 @@
+import io
 import pathlib
+import sys
+
+
+class CapturePrint:
+    """Captures text printed to the terminal when running commands"""
+    def __enter__(self):
+        self.terminal_stdout = io.StringIO()
+        sys.stdout = self.terminal_stdout
+        return self.terminal_stdout
+
+    def __exit__(self, *args, **kwargs):
+        sys.stdout = sys.__stdout__
+
 
 # Define variables available to all tests
 PROJECT_TEST_DIR = pathlib.Path(__file__).resolve().parent
 SAMPLE_FILES_DIR = PROJECT_TEST_DIR / 'sample_files'
+
 
 # Import and run tests
 from .dictionary import *
