6df96b1aec56f0e985e55fae908a76ab8e24b1c7
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-07-01 23:15:52 -0400

    Create function to check that lengths of objects are equal

--- mahautils/utils/vartools.py
+++ mahautils/utils/vartools.py
@@ -49,3 +49,37 @@ def max_list_item_len(input_list: Union[list, tuple]):
         Length of item in list with maximum length
     """
     return max(list(map(len, input_list)))
+
+
+def check_len_equal(item1: Any, item2: Any, *args: Any):
+    """Checks whether the lengths of a set of lists or tuples are equal
+
+    Evaluates the lengths of a set of items (such as lists, tuples, or
+    strings), returning whether all items have the same length as well as
+    either the length of all items (if all lengths are equal) or a list
+    containing the lengths of each item (if they are not equal)
+
+    Parameters
+    ----------
+    item1 : Any
+        First item whose length to evaluate
+    item2 : Any
+        Second item whose length to evaluate
+    *args : Any, optional
+        Any other items whose lengths are to be evaluated
+
+    Returns
+    -------
+    bool
+        Whether all items have the same length
+    int or list
+        Returns an integer containing the length of all items (if all
+        lengths are equal), or a list containing the lengths of each
+        item (if lengths differ)
+    """
+    lengths = [len(item1)] + [len(item2)] + [len(i) for i in args]
+
+    if len(set(lengths)) == 1:
+        return True, lengths[0]
+    else:
+        return False, lengths
--- tests/utils/vartools.py
+++ tests/utils/vartools.py
@@ -3,6 +3,7 @@ import unittest
 from mahautils.utils.vartools import (
     convert_to_tuple,
     max_list_item_len,
+    check_len_equal,
 )
 
 
@@ -75,3 +76,107 @@ class Test_MaxListLength(unittest.TestCase):
         # maximum length of a list with types whose lengths are not defined
         with self.assertRaises(TypeError):
             max_list_item_len([2, 'string'])
+
+
+class Test_CheckLenEqual(unittest.TestCase):
+    def test_check_2_list_equal(self):
+        # Verifies that lengths of two lists are evaluated correctly
+        self.assertTupleEqual(
+            check_len_equal(['a', 'b', 'c'], [1, 2, 3]),
+            (True, 3)
+        )
+        self.assertTupleEqual(check_len_equal(['a'], [1]), (True, 1))
+
+    def test_check_3_list_equal(self):
+        # Verifies that lengths of three lists are evaluated correctly
+        self.assertTupleEqual(
+            check_len_equal(['a', 'b'], [1, 2], ['cd', 3]),
+            (True, 2)
+        )
+        self.assertTupleEqual(check_len_equal(['a'], [1], ['c']), (True, 1))
+
+    def test_check_4_list_equal(self):
+        # Verifies that lengths of four lists are evaluated correctly
+        self.assertTupleEqual(
+            check_len_equal(['a', 'b'], [1, 2], ['cd', 3], [None, None]),
+            (True, 2)
+        )
+        self.assertTupleEqual(check_len_equal(['a'], [1], ['c'], [None]), (True, 1))
+
+    def test_check_2_tuple_equal(self):
+        # Verifies that lengths of two tuples are evaluated correctly
+        self.assertTupleEqual(
+            check_len_equal(('a', 'b', 'c'), (1, 2, 3)),
+            (True, 3)
+        )
+        self.assertTupleEqual(check_len_equal(('a',), (1,)), (True, 1))
+
+    def test_check_3_tuple_equal(self):
+        # Verifies that lengths of three tuples are evaluated correctly
+        self.assertTupleEqual(
+            check_len_equal(('a', 'b'), (1, 2), ('cd', 3)),
+            (True, 2)
+        )
+        self.assertTupleEqual(check_len_equal(('a',), (1,), ('cd',)), (True, 1))
+
+    def test_check_4_tuple_equal(self):
+        # Verifies that lengths of four tuples are evaluated correctly
+        self.assertTupleEqual(
+            check_len_equal(('a', 'b'), (1, 2), ('cd', 3), (None, None)),
+            (True, 2)
+        )
+        self.assertTupleEqual(check_len_equal(('a',), (1,), ('cd',), (None,)), (True, 1))
+
+    def test_check_2_str_equal(self):
+        # Verifies that lengths of two strings are evaluated correctly
+        self.assertTupleEqual(
+            check_len_equal('abc', '123'),
+            (True, 3)
+        )
+
+    def test_check_3_str_equal(self):
+        # Verifies that lengths of three strings are evaluated correctly
+        self.assertTupleEqual(
+            check_len_equal('ab', '12', 'c3'),
+            (True, 2)
+        )
+
+    def test_check_4_str_equal(self):
+        # Verifies that lengths of four strings are evaluated correctly
+        self.assertTupleEqual(
+            check_len_equal('ab', '12', 'c3', 'No'),
+            (True, 2)
+        )
+
+    def test_check_mixed_type_equal(self):
+        # Verifies that lengths of mixed list/tuple/string arguments
+        # are evaluated correctly
+        self.assertTupleEqual(
+            check_len_equal(['a', 'b'], (1, 2), ('cd', 3), (None, None), 'ce'),
+            (True, 2)
+        )
+        self.assertTupleEqual(check_len_equal(('a',), (1,), ('cd',), (None,), 'c'), (True, 1))
+
+    def test_check_2_mixed_type_unequal(self):
+        # Verifies that lengths of 2 mixed list/tuple/string arguments with
+        # different lengths are compared correctly
+        self.assertTupleEqual(
+            check_len_equal((1, 2), 'abcdefjkl'),
+            (False, [2, 9])
+        )
+
+    def test_check_3_mixed_type_unequal(self):
+        # Verifies that lengths of 3 mixed list/tuple/string arguments with
+        # different lengths are compared correctly
+        self.assertTupleEqual(
+            check_len_equal(('cd', 3), (None, None), 'abcdefjkl'),
+            (False, [2, 2, 9])
+        )
+
+    def test_check_5_mixed_type_unequal(self):
+        # Verifies that lengths of 5 mixed list/tuple/string arguments with
+        # different lengths are compared correctly
+        self.assertTupleEqual(
+            check_len_equal(['a', 'b', 'c'], (1, 2), ('cd', 3), (None, None), 'abcdefjkl'),
+            (False, [3, 2, 2, 2, 9])
+        )
