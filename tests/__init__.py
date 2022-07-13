98db1b8f64b8e9c4004ffa69d48db247145c13f2
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-07-12 22:57:39 -0400

    Create context manager to create temporary folders for testing
    Set up context manager that creates a temporary folder tests can write files to during testing. Also added general testing temporary folder to `.gitignore` to prevent temporary files being tracked by Git

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,17 @@
 import io
+import os
 import pathlib
+import shutil
 import sys
 
 
+# Define variables available to all tests
+PROJECT_TEST_DIR = pathlib.Path(__file__).resolve().parent
+SAMPLE_FILES_DIR = PROJECT_TEST_DIR / 'sample_files'
+TEST_TMP_DIR = PROJECT_TEST_DIR / 'tmp'
+
+
+# Define context managers to facilitate testing
 class CapturePrint:
     """Captures text printed to the terminal when running commands"""
     def __enter__(self):
@@ -14,9 +23,23 @@ class CapturePrint:
         sys.stdout = sys.__stdout__
 
 
-# Define variables available to all tests
-PROJECT_TEST_DIR = pathlib.Path(__file__).resolve().parent
-SAMPLE_FILES_DIR = PROJECT_TEST_DIR / 'sample_files'
+class CreateTempTestDir:
+    """Sets up temporary folder for reading/writing test files"""
+    def __enter__(self):
+        # Set test directory name
+        index = 0
+        while (test_dir := (TEST_TMP_DIR / f'test{index}')).exists():
+            index += 1
+
+        self.test_dir = test_dir
+
+        # Create test directory
+        os.makedirs(self.test_dir)
+
+        return self.test_dir
+
+    def __exit__(self, *args, **kwargs):
+        shutil.rmtree(self.test_dir)
 
 
 # Import and run tests
