211eb1e1953903cb7804c7c0aec6b20a30ad7af6
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-08-13 08:28:47 -0400

    Remove existing package code

--- mahautils/__init__.py
+++ mahautils/__init__.py
@@ -1,4 +1,9 @@
 """Package for analyzing Maha Multics software files"""
 
-from . import exceptions
-from . import parsers
+
+# PROGRAM VERSION ------------------------------------------------------------
+_VERSION_MAJOR = 0
+_VERSION_MINOR = 0
+_VERSION_PATCH = 0
+
+__version__ = f'{_VERSION_MAJOR}.{_VERSION_MINOR}.{_VERSION_PATCH}'
--- mahautils/exceptions.py
+++
@@ -1,5 +0,0 @@
-"""Custom Exceptions"""
-
-
-class InvalidFileFormat(Exception):
-    """Raise an error about invalid file format"""
