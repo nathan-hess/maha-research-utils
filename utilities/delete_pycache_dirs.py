9faea9e7db3e6b0608d88c4c3018815aef259b34
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-08-13 08:32:50 -0400

    Create utility script to delete `__pycache__/` directories

---
+++ utilities/delete_pycache_dirs.py
@@ -0,0 +1,19 @@
+import pathlib
+import shutil
+
+# Get location of project directory
+BASE_DIR = pathlib.Path(__file__).resolve().parents[1]
+
+# Get location of any `__pycache__/` directories
+pycache_dirs = [path for path in BASE_DIR.glob('**/__pycache__/') if path.is_dir()]
+num_pycache_dirs = len(pycache_dirs)
+
+# Remove `__pycache__/` directories
+if num_pycache_dirs > 0:
+    print(f'Removing {num_pycache_dirs} `__pycache__/` directories...')
+
+    for path in pycache_dirs:
+        print(f'  {path}')
+        shutil.rmtree(path)
+else:
+    print('Did not find any `__pycache__/` directories to remove')
