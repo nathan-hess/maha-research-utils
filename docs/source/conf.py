b7b7aa79b428cb7179d474dcdd693401b04de219
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-08-13 09:03:15 -0400

    Update Sphinx configuration options
    Modified basic Sphinx documentation configuration settings in `conf.py`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,26 +3,53 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import datetime
+import pathlib
+import sys
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[2]))
+import mahautils
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'MahaUtils'
-copyright = '2022, Nathan Hess'
 author = 'Nathan Hess'
-release = '0.0.0'
+copyright = f'{datetime.datetime.now().year}'
+release = mahautils.__version__
+url = 'https://github.com/nathan-hess/maha-research-utils'
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+# Add any Sphinx extension module names here, as strings
 extensions = []
 
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-exclude_patterns = []
 
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# HTML theme
 html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# General HTML options
+html_title = f'{project} v{release}'
+html_last_updated_fmt = '%b %d, %Y'
+html_permalinks = False
+html_show_sourcelink = False
+html_show_sphinx = False
