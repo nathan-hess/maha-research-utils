84e504b67f65c0c046feeef11259dd10a92598b5
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-08-26 22:15:21 -0400

    Remove non-functional line attempting to sort tests
    Removed line in script that runs Python tests intended to run tests in order, as unit tests should be independent and capable of being run in any order

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,6 @@
 import unittest
 from tests import *
 
-# Run tests in order
-unittest.TestLoader.sortTestMethodsUsing = None
-
 # Run tests
 if __name__ == '__main__':
     unittest.main(verbosity=2, catchbreak=True)
