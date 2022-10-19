b106ee7c0a28826f916ddcd65be9a64ee2f469f2
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-10-19 16:38:31 -0400

    Create classes for defining arbitrary points
    Added new classes to `mahautils.shape_designer.shapes` module that allow users to define points in space. A generic base class `Point` was created, as well as a derived class `CartesianPoint2D` specifically intended for creating points in a 2D Cartesian coordinate system

---
+++ mahautils/shape_designer/shapes/__init__.py
@@ -0,0 +1,10 @@
+"""**Shapes that can be used when constructing geometry with the MahaUtils
+ShapeDesigner**
+
+This module provides geometric shapes that can be used when creating more
+complex geometry with the ShapeDesigner module.  The specification of the
+shapes in this module is intended to be as generic and flexible as possible
+to allow greater freedom in building complex geometry.
+"""
+
+from .point import CartesianPoint2D, Point
