f03c0e51cb67be44e91063493d978596fabb35e4
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-12-11 21:57:07 -0500

    Create new module `mahautils.multics` for Maha Multics file parsers

---
+++ mahautils/multics/__init__.py
@@ -0,0 +1,6 @@
+"""**Classes for parsing Maha Multics input and output files**
+
+The ``mahautils.multics`` module provides file parsers compatible with Maha
+Multics input and output files.  These objects are intended to aid in pre-
+and post-processing tasks.
+"""
