cd080b0aa1cf754f19d2d8a46fe5dd66df3eca25
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2023-03-24 10:23:25 -0400

    Create `__main__.py` file
    Created `__main__.py` file to allow MahaUtils tools to be run with `python3 -m mahautils [COMMAND]`

---
+++ mahautils/__main__.py
@@ -0,0 +1,28 @@
+"""Runs MahaUtils CLI and GUI tools
+
+This module allows MahaUtils tools to be run from the command line using:
+
+.. code-block:: shell
+
+    python3 -m mahautils [COMMAND]
+
+Where ``[COMMAND]`` is one of the following:
+
+- ``simviewer``: Simulation results viewer
+"""
+
+import sys
+
+from .multics.sim_results_viewer.app import main as run_sim_results_viewer
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('MahaUtils: error: No command provided')
+        sys.exit(1)
+
+    command = sys.argv[1]
+    if command.lower() == 'simviewer':
+        run_sim_results_viewer(sys.argv[2:])
+    else:
+        print(f'MahaUtils: error: Invalid command "{command}"')
