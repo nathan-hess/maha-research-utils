9dd6ca24e9c4035816e2003b0914a4b614c78aa2
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2023-04-01 20:45:28 -0400

    Move plot configuration default values to `dcc.Store` storage
    Saved default values for plot settings in the configuration dictionaries rather than hard-coding the values in the plot generation function. This allows other app elements to access these defaults (for instance, to generate UI elements)

--- mahautils/multics/sim_results_viewer/plotting.py
+++ mahautils/multics/sim_results_viewer/plotting.py
@@ -11,7 +11,7 @@ import plotly.graph_objects as go  # type: ignore
 
 from mahautils.multics.sim_results_viewer.constants import SIM_RESULTS_DICT_T
 
-uirevision_counter = itertools.count(1)
+uirevision_counter = itertools.count(2)
 
 
 def graph():
@@ -36,19 +36,19 @@ def update_graph(config_general: dict, config_x: dict, config_y: dict,
     """Generates the plot of simulation results based on the current
     user-specified configuration options"""
     figure = go.Figure()
-    append_units = bool(config_general.get('append_units', False))
+    append_units = bool(config_general['append_units'])
 
     ## Plot simulation results data ##
-    y_axes: List[Dict[str, Any]] = config_y.get('axes', [])
+    y_axes: List[Dict[str, Any]] = config_y['axes']
+    num_active_axes = sum(x['enabled'] for x in y_axes)
+    width_per_axis = float(config_y['width_per_axis'])
 
-    default_axes_enabled = True
-    num_active_axes = sum(x.get('enabled', default_axes_enabled) for x in y_axes)
-
-    width_per_axis = float(config_y.get('width_per_axis', 0.1))
-
-    if (len(y_axes) > 0) and ('variable' in config_x) and ('units' in config_x):
+    if (
+        len(y_axes) > 0
+        and (None not in (config_x['variable'], config_x['units']))
+    ):
         # Settings for x-axis
-        if (axis_title := config_x.get('axis_title', None)) is not None:
+        if (axis_title := config_x['axis_title']) not in (None, ''):
             if append_units:
                 axis_title += f' [{config_x["units"]}]'
 
@@ -57,14 +57,14 @@ def update_graph(config_general: dict, config_x: dict, config_y: dict,
         figure.update_layout(
             xaxis={'domain': [width_per_axis*(num_active_axes-1), 1]})
 
-        if (dtick := config_x.get('tick_spacing', None)) is not None:
+        if (dtick := config_x['tick_spacing']) != 'auto':
             figure.update_xaxes(dtick=dtick)
 
         # Settings for y-axes
         y_axis_data: Dict[str, Any]
         i = 0
         for y_axis_data in y_axes:
-            if not y_axis_data.get('enabled', default_axes_enabled):
+            if not y_axis_data['enabled']:
                 continue
 
             plot_data = {'yaxis': f'y{i+1 if i > 0 else ""}'}
@@ -133,10 +133,10 @@ def update_graph(config_general: dict, config_x: dict, config_y: dict,
     figure.update_yaxes(zeroline=False)
 
     figure.update_layout(
-        plot_bgcolor=str(config_general.get('background_color', 'white')))
+        plot_bgcolor=config_general['background_color'])
 
     # Plot title
-    if config_general.get('title', None) is not None:
+    if config_general['title'] not in (None, ''):
         figure.update_layout(
             title={
                 'text': f'<b>{config_general["title"]}</b>',
@@ -148,19 +148,21 @@ def update_graph(config_general: dict, config_x: dict, config_y: dict,
         )
 
     # Gridlines
-    figure.update_xaxes(showgrid=bool(config_general.get('grid_x', True)))
-    figure.update_yaxes(showgrid=bool(config_general.get('grid_y', True)))
+    figure.update_xaxes(showgrid=bool(config_general['grid_x']))
+    figure.update_yaxes(showgrid=bool(config_general['grid_y']))
 
     # Cursor hover settings
     figure.update_layout(
-        hovermode=config_general.get('hovermode', 'closest'),
+        hovermode=config_general['hovermode'],
         hoverlabel={'namelength': -1},  # prevents truncating hover text
     )
 
     # Whether to change settings like zoom and which legend entries are active
     # when figure is updated by Dash
-    if config_general.get('freeze_uirevision', False):
-        figure.update_layout(uirevision=0)
+    if config_general['freeze_uirevision']:
+        # For the plot UI state not to be updated, "uirevision" must be set to
+        # a constant, nonzero value (plotly/graphing-library-docs#116)
+        figure.update_layout(uirevision=1)
     else:
         figure.update_layout(uirevision=next(uirevision_counter))
 
--- mahautils/multics/sim_results_viewer/store.py
+++ mahautils/multics/sim_results_viewer/store.py
@@ -33,10 +33,20 @@ def file_metadata_store():
 def plot_config_general_store():
     """Creates browser session storage for general plot settings
     """
+    default_data = {
+        'title': None,
+        'background_color': 'white',
+        'grid_x': True,
+        'grid_y': True,
+        'append_units': False,
+        'hovermode': 'closest',
+        'freeze_uirevision': False,
+    }
+
     contents = dash.dcc.Store(
         id='plot-config-general-store',
         storage_type='session',
-        data={},
+        data=default_data,
     )
 
     return contents
@@ -45,10 +55,17 @@ def plot_config_general_store():
 def plot_config_x_store():
     """Creates browser session storage for x-axis plot settings
     """
+    default_data = {
+        'axis_title': None,
+        'variable': None,
+        'units': None,
+        'tick_spacing': 'auto',
+    }
+
     contents = dash.dcc.Store(
         id='plot-config-x-store',
         storage_type='session',
-        data={},
+        data=default_data,
     )
 
     return contents
@@ -57,10 +74,15 @@ def plot_config_x_store():
 def plot_config_y_store():
     """Creates browser session storage for y-axes plot settings
     """
+    default_data = {
+        'width_per_axis': 0.1,
+        'axes': [],
+    }
+
     contents = dash.dcc.Store(
         id='plot-config-y-store',
         storage_type='session',
-        data={},
+        data=default_data,
     )
 
     return contents
