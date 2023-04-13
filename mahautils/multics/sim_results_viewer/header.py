a5b971c2a317122d5be6c58641aea545efe805a7
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2023-04-13 15:04:51 -0400

    Add button to open another SimViewer instance in a new tab

--- mahautils/multics/sim_results_viewer/header.py
+++ mahautils/multics/sim_results_viewer/header.py
@@ -7,6 +7,14 @@ import dash_bootstrap_components as dbc  # type: ignore
 
 from .constants import GUI_SHORT_NAME, PROJECT_NAME, VERSION
 
+button_style = {
+    'borderRadius': '8px',
+    'marginTop': '5px',
+    'marginLeft': '6px',
+    'marginRight': '2px',
+    'marginBottom': '5px',
+}
+
 
 def app_header():
     """Creates a formatted header for the application"""
@@ -33,33 +41,40 @@ def app_header():
                             dash.html.I(className='fa fa-wrench'),
                             id='plot-config-button',
                             size='sm',
-                            style={
-                                'borderRadius': '8px',
-                                'marginTop': '5px',
-                                'marginLeft': '10px',
-                                'marginRight': '2px',
-                                'marginBottom': '5px',
-                            },
+                            style=button_style,
                         ),
                         dbc.Tooltip(
                             f'Configure {GUI_SHORT_NAME} settings',
                             target='plot-config-button',
+                            trigger='hover',
                         ),
                         dbc.Button(
                             dash.html.I(className='fa fa-question-circle'),
                             id='info-button',
                             size='sm',
-                            style={
-                                'borderRadius': '8px',
-                                'marginTop': '5px',
-                                'marginLeft': '2px',
-                                'marginRight': '10px',
-                                'marginBottom': '5px',
-                            },
+                            style=button_style,
                         ),
                         dbc.Tooltip(
                             f'View {GUI_SHORT_NAME} version and acknowledgments',
                             target='info-button',
+                            trigger='hover',
+                        ),
+                        dash.html.A(
+                            dbc.Button(
+                                dash.html.I(
+                                    className='fa fa-up-right-from-square'
+                                ),
+                                id='new-instance-button',
+                                size='sm',
+                                style=button_style,
+                            ),
+                            href='',
+                            target='_blank',
+                        ),
+                        dbc.Tooltip(
+                            f'Open a new {GUI_SHORT_NAME} instance',
+                            target='new-instance-button',
+                            trigger='hover',
                         ),
                     ],
                 ),
