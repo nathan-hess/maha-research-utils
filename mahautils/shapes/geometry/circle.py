b8052670081420a11b75976abc37e5a6cef07115
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2023-04-27 22:19:36 -0400

    Implement `Circle.rotate()` method

--- mahautils/shapes/geometry/circle.py
+++ mahautils/shapes/geometry/circle.py
@@ -210,6 +210,24 @@ class Circle(ClosedShape2D):
             repeat_end=repeat_end,
         )
 
+    def rotate(self, center: Union[Array_Float2, CartesianPoint2D],
+               angle: float, angle_units: str = 'rad') -> None:
+        # Convert angle to radians
+        angle = self._convert_rotate_angle(angle, angle_units)
+
+        # Shift geometry to origin
+        center = CartesianPoint2D(center)
+        x_offset = self.center.x - center.x
+        y_offset = self.center.y - center.y
+
+        # Rotate center about origin
+        self.center.x = x_offset*math.cos(angle) - y_offset*math.sin(angle)
+        self.center.y = x_offset*math.sin(angle) + y_offset*math.cos(angle)
+
+        # Shift geometry back to center of rotation
+        self.center.x += center.x
+        self.center.y += center.y
+
     def translate(self, x: float = 0, y: float = 0) -> None:
         self.center.x += x
         self.center.y += y
