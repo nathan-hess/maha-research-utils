22e55af390669038981eb01b7581a382436467e5
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2023-06-26 15:34:52 -0400

    Display additional simulation results metadata in SimViewer

--- mahautils/multics/sim_results_viewer/panel/utils_generate_table.py
+++ mahautils/multics/sim_results_viewer/panel/utils_generate_table.py
@@ -26,23 +26,70 @@ def generate_file_table_body(sim_results_files: SIM_RESULTS_DICT_T,
     """Populates the simulation results file table with all currently uploaded
     simulation results files"""
     description_style = {
-        'fontSize': 12,
+        'fontSize': 12.5,
         'marginTop': 0,
         'marginBottom': 0,
         'paddingTop': 0,
         'paddingBottom': 0,
     }
 
+    description_header_style = description_style.copy()
+    description_header_style['fontSize'] += 1.5
+
     contents = []
     for _, key in enumerate(sim_results_files.keys()):
         display_metadata = []
-        if (title := sim_results_files[key].title) is not None:
+
+        title = sim_results_files[key].title
+        sim_version = sim_results_files[key].sim_version
+        if (title is not None) or (sim_version is not None):
             display_metadata.append(
-                dash.html.P(
-                    dash.html.I(f'Title: {title}'),
-                    style=description_style,
-                )
+                dash.html.B('Simulation Metadata',
+                            style=description_header_style)
             )
+
+            if title is not None:
+                display_metadata.append(
+                    dash.html.P(
+                        [dash.html.B('Title: '), title],
+                        style=description_style,
+                    )
+                )
+
+            if sim_version is not None:
+                display_metadata.append(
+                    dash.html.P(
+                        [dash.html.B('Version: '), sim_version],
+                        style=description_style,
+                    )
+                )
+
+        maha_multics_version = sim_results_files[key].maha_multics_version
+        maha_multics_commit = sim_results_files[key].maha_multics_commit
+        if (maha_multics_version is not None) or (maha_multics_commit is not None):
+            display_metadata.append(dash.html.P())
+            display_metadata.append(
+                dash.html.B('Maha Multics Metadata',
+                            style=description_header_style)
+            )
+
+            if maha_multics_version is not None:
+                display_metadata.append(
+                    dash.html.P(
+                        [dash.html.B('Version: '),
+                         maha_multics_version],
+                        style=description_style,
+                    )
+                )
+
+            if maha_multics_commit is not None:
+                display_metadata.append(
+                    dash.html.P(
+                        [dash.html.B('Commit: '),
+                         maha_multics_commit],
+                        style=description_style,
+                    )
+                )
 
         if len(display_metadata) == 0:
             file_id_description = dash.html.H6(key)
